88125fea2035a808406dbc71f68d928b7414985c
iptv-cloud <95514761+iptv-cloud@users.noreply.github.com>
2022-02-07 16:15:56 -0500

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,12 @@
+function soluntion(N) {
+  for (let i = 0; i < N-1; i++) {
+    process.stdout.write('L\n');
+  }
+    for (let i = 0; i < N; i++) {
+    process.stdout.write('L');
+  }
+}
+
+
+const N=4;
+soluntion(N);
